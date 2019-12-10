55f87f09bc45516e62fd3d3e38b3f6fffdcd14ea
dashachernyh <dasha.chernyh.@mail.ru>
2019-12-11 00:21:25 +0300

    hhh

--- MyForm.h
+++ MyForm.h
@@ -1,5 +1,8 @@
 #pragma once
-
+#include<msclr\marshal_cppstd.h>
+#include"calcul.h"
+#include"stack.h"
+#include<string>
 namespace CppWinForm1 {
 
 	using namespace System;
@@ -34,6 +37,11 @@ namespace CppWinForm1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
 
 	private:
 		/// <summary>
@@ -48,12 +56,73 @@ namespace CppWinForm1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(73, 105);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(195, 103);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"=";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(298, 79);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(75, 17);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"результат";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(310, 110);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(46, 17);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"label2";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(385, 331);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		std::string infix,postfix;
+		infix = msclr::interop::marshal_as<std::string>(textBox1->Text);
+		TCalculator<double> calc;
+		calc.SetExpr(infix);
+		calc.ToPosfix();
+		postfix = calc.GetPostfix();
+		double rez = calc.Calc();
+		label1->Text = Convert::ToString(rez);
+		label2->Text = gcnew String(postfix.c_str());
+	}
 	};
 }
---
+++ calcul.h
@@ -0,0 +1,148 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include"stack.h"
+#include<string>
+#include<cmath>
+template<class T>
+class TCalculator
+{
+	string expr, postfix;
+	TStack<char> st_c;
+	TStack<double>st_d;
+	int Prior(char c); //функция
+public:
+	TCalculator() :st_c(50) {}
+	void SetExpr(string _expr);
+	string GetExpr();
+	string GetPostfix();
+	bool CheckBrackets() const;
+	void ToPosfix();
+	double Calc();
+};
+template<class T>
+void TCalculator<T>::SetExpr(string _expr)
+{
+	expr = _expr;
+}
+template<class T>
+string TCalculator<T>::GetExpr()
+{
+	return expr;
+}
+template<class T>
+string TCalculator<T>::GetPostfix()
+{
+	return postfix;
+}
+template<class T>
+bool TCalculator<T>::CheckBrackets()const
+{
+	st_c.ClearSt();
+	for (int i = 0; i < expr.size(); i++)
+	{
+		if (expr[i] == '(')
+			st_c.Push('(');
+		if (expr[i] == ')')
+		{
+			if (st_c.IsEmpty()) return false;
+			else st_c.Pop();
+		}
+	}
+	if (st_c.IsEmpty()) return true;
+	else return false;
+}
+template<class T>
+int TCalculator<T>::Prior(char c)
+{
+	if (c == '(') return 0;
+	if (c == '+' || c == '-') return 1;
+	if (c == '*' || c == '/') return 2;
+	if (c == '^') return 3;
+}
+template<class T>
+void TCalculator<T>::ToPosfix()
+{
+	string str = "(";
+	str += expr;
+	str += ")";
+	st_c.ClearSt();
+	postfix = " ";
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == ' ') continue;
+		if (str[i] == '(') st_c.Push('(');
+		if ((str[i] >= '0' && str[i] <= '9') || str[i] == '.') postfix += str[i];
+		if (str[i] == ')')
+		{
+			char tmp = st_c.Pop();
+			while (tmp != '(')
+			{
+				postfix += ' ';
+				postfix += tmp;
+				tmp = st_c.Pop();
+			}
+		}
+		if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '^')
+		{
+			postfix += ' ';
+			char tmp = st_c.Pop();
+			while (Prior(str[i]) <= Prior(tmp))
+			{
+
+				postfix += tmp;
+				tmp = st_c.Pop();
+			}
+
+			st_c.Push(tmp);
+			st_c.Push(str[i]);
+		}
+	}
+}
+template<class T>
+double TCalculator<T>::Calc()
+{
+	st_d.ClearSt();
+	char*tmp;
+	double rez;
+	for (int i = 0; i < postfix[i]; i++)
+	{
+		if (postfix[i] == ' ') continue;
+		if (postfix[i] >= '0'&&postfix[i] <= '9')
+		{
+			double d = strtod(&postfix[i], &tmp);
+			int j = tmp - &postfix[i];
+			i += j - 1;
+			st_d.Push(d);
+		}
+		if (postfix[i] == '+' || postfix[i] == '-' || postfix[i] == '*' || postfix[i] == '/' || postfix[i] == '^')
+		{
+			double op1, op2;
+			op2 = st_d.Pop();
+			op1 = st_d.Pop();
+			if (postfix[i] == '+')
+			{
+				rez = op1 + op2;
+			}
+			if (postfix[i] == '-')
+			{
+				rez = op1 - op2;
+			}
+			if (postfix[i] == '*')
+			{
+				rez = op1 * op2;
+			}
+			if (postfix[i] == '/')
+			{
+				rez = op1 / op2;
+			}
+			if (postfix[i] == '^')
+			{
+				rez = pow(op1, op2);
+			}
+			st_d.Push(rez);
+		}
+	}
+	rez = st_d.Pop();
+	return rez;
+}
